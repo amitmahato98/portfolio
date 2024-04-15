85ef9b2d0e8432dad3b5ca82d408835df2860bc8
amitmahato98 <shankhakush@gmail.com>
2024-04-15 20:15:11 +0545

    project page is added

--- currency.js
+++ currency.js
@@ -19,21 +19,20 @@ fetch(`https://${host}/currencies`)
 
 function converter(){
   var inputamountVal = inputamount.value;
-  if (select[0].value !== select[1].value) {
-    const host = 'api.frankfurter.app';
-    fetch(`https://${host}/latest?amount=${inputamountVal}&from=${select[0].value}&to=${select[1].value}`)
-    .then((val) => val.json())
-    .then((val) => {
-        alert(val)
-        console.log(val);
-        document.write(val)
-        outputamount.value = Object.values(val.rates)[0];
-    })
-    .catch(error => {
-        console.error('Error fetching exchange rates:', error);
+  
+ if(select[0].value != select[1].value){
+  const host = 'api.frankfurter.app';
+  fetch(`https://${host}/latest?amount=${inputamountVal}&from=${select[0].value}&to=${select[1].value}`)
+  .then(val => val.json())
+  .then((val) => {
+    // alert(val)
+    console.log(val);
+    outputamount.value = Object.values(val)[0];
     });
-}
-
-else
+}else
   alert('wrong');
 }
+
+// url='https://api.currencyapi.com/v3/latest?apikey=cur_live_HJdtayeaHA6U1EmXX76G3DVlQUxAtAH5U206pdEM'
+
+// co/nst
